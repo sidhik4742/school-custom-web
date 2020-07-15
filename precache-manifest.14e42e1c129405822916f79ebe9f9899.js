0115ad1678988a453f9c61eaef7ce76efc298422
sidhik4742 <sidhik64742@gmail.com>
2020-07-15 15:23:58 +0530

    Updates

---
+++ precache-manifest.14e42e1c129405822916f79ebe9f9899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e22c30ab150b72978a9120e05fb75db",
+    "url": "/school-custom-web/index.html"
+  },
+  {
+    "revision": "c465b974d3b22b3c9126",
+    "url": "/school-custom-web/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d297d3b8f0363357e1ac",
+    "url": "/school-custom-web/static/css/main.01dd4ffd.chunk.css"
+  },
+  {
+    "revision": "c465b974d3b22b3c9126",
+    "url": "/school-custom-web/static/js/2.c35ec8ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/school-custom-web/static/js/2.c35ec8ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d297d3b8f0363357e1ac",
+    "url": "/school-custom-web/static/js/main.42976541.chunk.js"
+  },
+  {
+    "revision": "f8904403685b00ded345",
+    "url": "/school-custom-web/static/js/runtime-main.af65afd4.js"
+  }
+]);
